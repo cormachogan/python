800ecff4fc5e161862f6c9bf95ac91780c044580
Cormac Hogan <chogan@vmware.com>
2021-03-31 11:32:07 +0100

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,26 @@
+#
+# -- To run:
+#
+# c = circle(7)
+# print('Area', c.area())
+# print('Diameter', c.diameter())
+# print('Perimeter', c.perimeter())
+#
+class circle:
+  """
+  A circle object is created from a radius, default is 1
+  """
+  pi = 3.141529
+  def __init__(self, radius=1):
+    "Initialise the circle from the specified radius"
+    self.radius = radius
+  def diameter(self):
+    "Return twice the radius"
+    return self.radius * 2
+  def area(self):
+    "calculate the area of the circle"
+    return circle.pi * self.radius **2
+  def perimeter(self):
+    "calculate the perimeter of the circle"
+    return self.diameter() * circle.pi
+    
